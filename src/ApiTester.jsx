e7bef4c2f363d2d148e5dddd499e0b75be902396
ConnorCoderAcademy <13716@coderacademy.edu.au>
2023-12-02 10:57:08 +1100

    start defining global statefor user data

--- src/ApiTester.jsx
+++ src/ApiTester.jsx
@@ -2,11 +2,12 @@ import { useContext } from "react"
 import { ConnectionContext } from "./context/ConnectionContext"
 
 export default function ApiTester() {
+    //let connectionData = useContext(ConnectionContext);
     let {url} = useContext(ConnectionContext);
+
     return(
         <div>
-            {/* <h1>API URL: {connectionData.connection}</h1> */}
-            <h1>API URL: {url}</h1>
+            <h1>API URL: {connectionData.u}</h1>
         </div>
     )
 
